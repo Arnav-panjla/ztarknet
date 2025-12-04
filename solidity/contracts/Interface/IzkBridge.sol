53beeb7e06169cedd15c5eeadb5648dbf33d297b
Arnav-panjla <arnavpanjla@gmail.com>
2025-12-05 04:00:51 +0530

    removed_solidity

--- solidity/contracts/Interface/IzkBridge.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// pragma solidity >= 0.8.0;
-pragma solidity ^0.6.11;
-
-interface IzkBridge {
-    // returns the merkle root
-    function BlockHeader(uint256 blockNumber) external view returns (bytes memory);
-}
